52b45c212aae58a7581310a935b3bfd9e4f6547f
Deivison06 <santosdeivison764@gmail.com>
2022-05-10 14:10:54 -0300

    Inseção do form com db

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="main-banner">
+        <h1>Your Burger</h1>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+    #main-banner {
+    background-image: url('/public/img/burger.jpg');
+    background-position: 0 -250px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  #main-banner h1 {
+    color: rgba(233, 233, 233, 0.856);
+    text-align: center;
+    font-size: 60px;
+    background-color: rgba(34, 34, 34, 0.623);
+    padding: 20px 40px;
+  }
+    
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="main-container">
-    <h1>Home</h1>
+<div>
+    <Banner /> 
+    <div class="main-container">
+      <h1>BUILD YOUR BURGER</h1>
+      <BurgeForm />
+    </div>
   </div>
 </template>
 
 <script>
-
+import Banner from '../components/Banner.vue'
+import BurgeForm from '../components/Formulario.vue'
 export default {
-
+  name: "Home",
+  components: {
+    Banner,
+    BurgeForm
+  }
 }
 </script>
